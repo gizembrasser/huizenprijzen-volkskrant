8f28a4db1226443329becabffada444fb074f450
unknown <gizembrasser101@gmail.com>
2024-10-20 09:02:22 +0200

    new csv file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
 links_file_path = os.path.join(data_folder, 'pararius_links.txt')
-csv_file_path = os.path.join(data_folder, 'pararius_listings.csv')
+csv_file_path = os.path.join(data_folder, 'pararius_listings2.csv')
 
 driver.get("https://www.pararius.com/apartments/nederland")
 
